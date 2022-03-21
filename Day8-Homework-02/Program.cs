0101863d1a9dd29886938300606fdb82c02d4953
Linda Šulce <linda.sulce@gmail.com>
2022-03-21 09:46:29 +0200

    updated first lines

--- Day8-Homework-02/Program.cs
+++ Day8-Homework-02/Program.cs
@@ -1,8 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-
-/*int number1a = 45;
+﻿
+/*int number1 = 45;
 int number2 = 7;
 */
 double result = 0;
