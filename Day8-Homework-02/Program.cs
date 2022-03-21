f5b56189b6f3730e688ccfcc4e381d21a1161e99
Linda Šulce <linda.sulce@gmail.com>
2022-03-21 09:42:44 +0200

    Add project files.

---
+++ Day8-Homework-02/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+/*int number1a = 45;
+int number2 = 7;
+*/
+double result = 0;
+Console.WriteLine($"Please enter the number!");
+string number1a = Console.ReadLine();
+int number1 = int.Parse(number1a);
+Console.WriteLine($"Please enter another the number!");
+string number2a = Console.ReadLine();
+int number2 = int.Parse(number2a);
+
+Console.WriteLine($"Please enter the type of calculation you want to do!");
+
+string calcType1 = Console.ReadLine();
+char calcType = char.Parse(calcType1);
+
+switch (calcType)
+{
+    case '+':
+        result = number1 + number2;
+        Console.WriteLine($"{result}");
+        break;
+    case '-':
+        result = number1 - number2;
+        Console.WriteLine($"{result}");
+        break;
+    case '/':
+        result = number1 / number2;
+        Console.WriteLine($"{result}");
+        break;
+    case '*':
+        result = number1 * number2;
+        Console.WriteLine($"{result}");
+        break;
+    case '%':
+        result = number1 % number2;
+        Console.WriteLine($"{result}");
+        break;
+    case 'p':
+        Console.WriteLine($"Numbers are {number1} and {number2}");
+        break;
+    case 'b':
+        result = (Math.Max(number1, number2));
+        Console.WriteLine($"{result}");
+        break;
+    case 's':
+        result = (Math.Min(number1, number2));
+        Console.WriteLine($"{result}");
+        break;
+}
